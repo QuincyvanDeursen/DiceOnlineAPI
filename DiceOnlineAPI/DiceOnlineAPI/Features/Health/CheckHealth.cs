118a8dbfcc60fcf3fdfd8792318736e6cdd193e4
Quincy <Q.vandeursen@student.avans.nl>
2025-08-02 14:06:01 +0200

    docker

---
+++ DiceOnlineAPI/DiceOnlineAPI/Features/Health/CheckHealth.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+
+namespace DiceOnlineAPI.Features.Health
+{
+    public static class CheckHealth
+    {
+
+        public class Endpoint : ICarterModule
+        {
+            public void AddRoutes(IEndpointRouteBuilder app)
+            {
+                app.MapGet("/health", () => Results.Ok("I'm Alive!"))
+                    .WithName("CheckHealth");
+            }
+        }
+    }
+}
